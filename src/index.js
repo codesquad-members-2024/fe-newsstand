1a44c4223959436c9793b16fd68aaf1854ec89b4
임승현 <96limshyun@gmail.com>
2024-03-04 19:36:07 +0900

    fix: 현재 날짜 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 function NewsStand() {
     // 다음페이지를 누르면 24씩++ 해 보여주려는 변수
-    let newsListCount = 0
+    let newsListCount = 0;
+    const DAY = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
     const getLogoTemplat = () => {
-        const logoTemplate = []
+        const logoTemplate = [];
         for (let i = newsListCount; i < newsListCount + 24; i++) {
             logoTemplate.push(`
             <li class="list-${i}">
@@ -10,15 +11,38 @@ function NewsStand() {
             </li>
             `);
         }
-        return logoTemplate.join("")
+        return logoTemplate.join("");
     };
     const renderCompanyLogo = () => {
         const logoTemplate = getLogoTemplat();
         const companyDisplayBox = document.querySelector(".company-display");
-        companyDisplayBox.innerHTML = logoTemplate
+        companyDisplayBox.innerHTML = logoTemplate;
     };
-    return { renderCompanyLogo };
+
+    const getCurrentDate = () => {
+        const date = new Date();
+        return [
+            date.getFullYear(),
+            date.getMonth() + 1,
+            date.getDate(),
+            DAY[date.getDay()],
+        ];
+    };
+
+    const renderCurrentDate = () => {
+        const dateHtmlBox = document.querySelector(".date")
+        const [year, month, date, DAY] = getCurrentDate();
+        dateHtmlBox.innerHTML = `
+        ${year}. ${month}. ${date}. ${DAY}
+        `
+    };
+
+    const main = () => {
+        renderCompanyLogo();
+        renderCurrentDate();
+    };
+    return { main, renderCurrentDate };
 }
 
 const newsStand = NewsStand();
-newsStand.renderCompanyLogo();
+newsStand.main();
