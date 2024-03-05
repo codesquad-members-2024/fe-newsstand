4d1f59cfc217f295f524dad42e0aacaafe44be38
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-03-05 15:30:27 +0900

    feat: date script js파일 분리

---
+++ .github/date.js
@@ -0,0 +1,4 @@
+const daysOfWeek = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+const date = new Date();
+const dayName = daysOfWeek[date.getDay()];
+document.getElementById("date").innerHTML = `${date.toLocaleDateString()} ${dayName}`;
