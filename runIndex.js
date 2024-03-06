a69cf6ec263b9bcd31679c0a81bf520d7abf57dd
jueun-park98 <jooeun06161@gmail.com>
2024-03-06 21:15:25 +0900

    feat: 뉴스 제목 렌더링 기능 추가

--- runIndex.js
+++ runIndex.js
@@ -1,12 +1,54 @@
 const WEEK = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+const PRESS_NAME_TEXT = '<span class="press-text">연합뉴스</span>';
+const NEWS_TITLE_TEXT = '<span class="news-title-text"></span>';
+const MAX_NEWS_TITLE_INDEX = 10;
+const PREVIOUS_CLASS_NAME = 'auto-rolling-text prev';
+const CURRENT_CLASS_NAME = 'auto-rolling-text cur';
+const NEXT_CLASS_NAME = 'auto-rolling-text next';
 
 const leftArrowButton = document.querySelector(".left-arrow-button");
 const rightArrowButton = document.querySelector(".right-arrow-button");
 
 let pressLogoTableIndex = 0;
+let newsTitleIndex = 1;
 
-const increaseIndex = () => {pressLogoTableIndex++;};
-const decreaseIndex = () => {pressLogoTableIndex--;};
+const increaseIndex = (index) => {index++;};
+const decreaseIndex = (index) => {index--;};
+
+const createNewNode = (className, titles) => {
+  const newNode = document.createElement("div");
+  const pressNameTag = new DOMParser().parseFromString(PRESS_NAME_TEXT, "text/xml").documentElement;
+  const newsTitleTag = new DOMParser().parseFromString(NEWS_TITLE_TEXT, "text/xml").documentElement;
+
+  newNode.classList.add(...className.split(" "));
+  newNode.appendChild(pressNameTag);
+  newsTitleTag.textContent += titles[newsTitleIndex];
+  newNode.appendChild(newsTitleTag);
+
+  newsTitleIndex++;
+  if (newsTitleIndex === MAX_NEWS_TITLE_INDEX) newsTitleIndex = 1;
+  return newNode;
+}
+
+const renderNewsTitle = async (tag, titles) => {
+  const texts = tag.getElementsByClassName("auto-rolling-text");
+  const prev = texts[0].childElementCount === 0 ? createNewNode(PREVIOUS_CLASS_NAME, titles) : texts[0];
+  const current = texts[1].childElementCount === 0 ? createNewNode(CURRENT_CLASS_NAME, titles) : texts[1];
+  const next = texts[2].childElementCount === 0 ? createNewNode(NEXT_CLASS_NAME, titles) : texts[2];
+  const newNext = createNewNode(NEXT_CLASS_NAME, titles);
+
+  if (texts[0].childElementCount === 0) tag.replaceChild(prev, texts[0]);
+  if (texts[1].childElementCount === 0) tag.replaceChild(current, texts[1]);
+  if (texts[2].childElementCount === 0) tag.replaceChild(next, texts[2]);
+
+  tag.removeChild(prev);
+  current.classList.remove('cur');
+  current.classList.add('prev');
+  next.classList.remove('next');
+  next.classList.add('cur');
+
+  tag.appendChild(newNext);
+}
 
 const setVisibilityOfArrowButtons = () => {
   if ( pressLogoTableIndex === 0 ) {
@@ -20,6 +62,28 @@ const setVisibilityOfArrowButtons = () => {
 
   leftArrowButton.style.visibility = "visible";
   rightArrowButton.style.visibility = "visible";
+}
+
+const renderCurrentDate = () => {
+  const currentDateTag = document.querySelector(".currentDate");
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const date = currentDate.getDate();
+  const day = WEEK[currentDate.getDay()];
+
+  currentDateTag.innerHTML = `${year}. ${month.toString().padStart(2, "0")}. ${date.toString().padStart(2, "0")}. ${day}`;
+}
+
+const renderNewsTitles = async () => {
+  const textBoxes = document.querySelectorAll(".auto-rolling-textbox");
+  const titlesPath = "data/news.json";
+  const response = await fetch(titlesPath);
+  const titles = await response.json().then((json) => json.titles);
+
+  textBoxes.forEach((textBox) => {
+    renderNewsTitle(textBox, titles);
+  });
 }
 
 const renderPressTable = async () => {
@@ -44,24 +108,17 @@ const renderPressTable = async () => {
   setVisibilityOfArrowButtons();
 }
 
-const renderCurrentDate = () => {
-  const currentDateTag = document.querySelector(".currentDate");
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1;
-  const date = currentDate.getDate();
-  const day = WEEK[currentDate.getDay()];
-
-  currentDateTag.innerHTML = `${year}. ${month.toString().padStart(2, "0")}. ${date.toString().padStart(2, "0")}. ${day}`;
-}
-
 renderCurrentDate();
+renderNewsTitles();
 renderPressTable();
 leftArrowButton.addEventListener('click', () => {
-  if ( pressLogoTableIndex > 0 ) decreaseIndex();
+  if ( pressLogoTableIndex > 0 ) decreaseIndex(pressLogoTableIndex);
   renderPressTable();
 });
 rightArrowButton.addEventListener('click', () => {
-  if ( pressLogoTableIndex < 3 ) increaseIndex();
+  if ( pressLogoTableIndex < 3 ) increaseIndex(pressLogoTableIndex);
   renderPressTable();
+});
+document.addEventListener('DOMContentLoaded', () => {
+  setInterval(renderNewsTitles, 4000); 
 });
